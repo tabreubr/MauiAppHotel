d5d68bd84200df06dd8c3214e89f011e1d09c107
Thiago Abreu <tabreu.br@gmail.com>
2025-05-20 21:30:00 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+    }
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Sobre());
+    }
+}
---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppHotel.Views;
+
+public partial class Sobre : ContentPage
+{
+    public Sobre()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+}
